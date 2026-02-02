9f217076747f69cea56b1a074e237f8f8863114b
Varun Dutt <varundutt@yahoo.com>
2026-02-02 09:45:10 +0530

    added power function

--- calculator.py
+++ calculator.py
@@ -10,6 +10,9 @@ def subtract(a, b):
 def multiply(a, b):
     return a * b
 
+# def power(a, b)
+#     return a**b
+
 def divide(a, b):
     if b == 0:
         return "Error: Division by zero"
@@ -21,3 +24,4 @@ if __name__ == "__main__":
     print("Subtraction of 10 and 5:", subtract(10, 5))
     print("Multiplication of 10 and 5:", multiply(10, 5))
     print("Division of 10 by 5:", divide(10, 5))
+    # print("Power of 2 to 5:", divide(2, 5))
